731df31de172a21501c46bd53bb4c6865bd34cbc
Jamie Tu <jd3main@gmail.com>
2020-12-08 03:52:54 +0800

    ER7 and FormulaMatrix

---
+++ Assets/Scripts/ER7.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class ER7 : MonoBehaviour
+{
+    public double[] theta = new double[6];
+    private const double scale = 0.01;
+    public Var c1 = new Var("c1");
+    public Var s1 = new Var("s1");
+    public Var c2 = new Var("c2");
+    public Var s2 = new Var("s2");
+    public Var c3 = new Var("c3");
+    public Var s3 = new Var("s3");
+    public Var c4 = new Var("c4");
+    public Var s4 = new Var("s4");
+    public Var c5 = new Var("c5");
+    public Var s5 = new Var("s5");
+    public Var d1 = new Var("d_1", 358.5);
+    public Var d5 = new Var("d_5", 251);
+    public Var a1 = new Var("a_1", 50);
+    public Var a2 = new Var("a_2", 300);
+    public Var a3 = new Var("a_3", 350);
+    public Transform[] joints = new Transform[6];
+
+    private FormulaMat T10;
+    private FormulaMat T21;
+    private FormulaMat T32;
+    private FormulaMat T43;
+    private FormulaMat T54;
+    private FormulaMat[] T;
+
+    void Start()
+    {
+        T10 = FormulaMat.Identity(4);
+        T10[0][0] = c1; T10[0][1] = -s1;
+        T10[1][0] = s1; T10[1][1] = c1;
+        T10[2][3] = d1;
+
+        T21 = new FormulaMat(4);
+        T21[0][0] = c2; T21[0][1] = -s2;    T21[0][3] = a1;
+        T21[1][2] = Formula.NegOne();
+        T21[2][0] = s2; T21[2][1] = c2;
+        T21[3][3] = Formula.One();
+
+        T32 = FormulaMat.Identity(4);
+        T32[0][0] = c3; T32[0][1] = -s3; T32[0][3] = a2;
+        T32[1][0] = s3; T32[1][1] = c3;
+
+        T43 = FormulaMat.Identity(4);
+        T43[0][0] = c4; T43[0][1] = -s4; T43[0][3] = a3;
+        T43[1][0] = s4; T43[1][1] = c4;
+
+        T54 = new FormulaMat(4);
+        T54[0][0] = c5; T54[0][1] = -s5;
+        T54[1][2] = Formula.NegOne(); T54[1][3] = -d5;
+        T54[2][0] = s5; T54[2][1] = c5;
+        T54[3][3] = Formula.One();
+
+        T = new FormulaMat[6];
+        T[0] = FormulaMat.Identity(4);
+        T[1] = T[0] * T10;
+        T[2] = T[1] * T21;
+        T[3] = T[2] * T32;
+        T[4] = T[3] * T43;
+        T[5] = T[4] * T54;
+
+        Debug.Log($"T10: {T10}");
+        Debug.Log($"T21: {T21}");
+        Debug.Log($"T32: {T32}");
+        Debug.Log($"T43: {T43}");
+        Debug.Log($"T54: {T54}");
+        Debug.Log($"T10*T21: {T10 * T21}");
+        Debug.Log($"T50: {T[5]}");
+        
+        Debug.Log($"T10: {ToLatex(T10)}");
+        Debug.Log($"T21: {ToLatex(T21)}");
+        Debug.Log($"T32: {ToLatex(T32)}");
+        Debug.Log($"T43: {ToLatex(T43)}");
+        Debug.Log($"T54: {ToLatex(T54)}");
+
+        for (int i = 0; i <= 5; i++)
+        {
+            Debug.Log($"T{i}0: {T[i]}");
+        }
+
+        FormulaMat T51 = T21 * T32 * T43 * T54;
+        FormulaMat T52 = T32 * T43 * T54;
+        FormulaMat T53 = T43 * T54;
+        Debug.Log($"T51: {T51}");
+        Debug.Log($"T52: {T52}");
+        Debug.Log($"T53: {T53}");
+    }
+
+    void Update()
+    {
+        c1.value = Math.Cos(theta[1]);
+        s1.value = Math.Sin(theta[1]);
+        c2.value = Math.Cos(theta[2]);
+        s2.value = Math.Sin(theta[2]);
+        c3.value = Math.Cos(theta[3]);
+        s3.value = Math.Sin(theta[3]);
+        c4.value = Math.Cos(theta[4]);
+        s4.value = Math.Sin(theta[4]);
+        c5.value = Math.Cos(theta[5]);
+        s5.value = Math.Sin(theta[5]);
+        Vector4 xUnit = new Vector4(1, 0, 0);
+        Vector4 yUnit = new Vector4(0, 1, 0);
+        Vector4 zUnit = new Vector4(0, 0, 1);
+        Vector4 origin = new Vector4(0, 0, 0, 1);
+
+        for (int i = 0; i < 6; i++)
+        {
+            Vector4 p = ToLeftHand(T[i] * origin);
+
+            joints[i].rotation = Quaternion.LookRotation(ToLeftHand(T[i] * yUnit), ToLeftHand(T[i] * zUnit));
+
+            joints[i].localPosition = p*(float)scale;
+        }
+    }
+
+
+    Vector4 ToLeftHand(Vector4 v)
+    {
+        return new Vector4(v.x, v.z, v.y, v.w);
+    }
+
+
+    private string ToLatex(FormulaMat m)
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine("\\begin{bmatrix}");
+        for (int i = 0; i < m.n; i++)
+        {
+            var line = new List<string>();
+            for (int j = 0; j < m.n; j++)
+            {
+                line.Add(m[i][j].ToString());
+            }
+            sb.Append(string.Join("&", line));
+            sb.Append("\\\\");
+        }
+        sb.Append("\\end{bmatrix}");
+        return sb.ToString();
+    }
+}
---
+++ Assets/Scripts/FormulaMat.cs
@@ -0,0 +1,321 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine.Assertions.Comparers;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Var
+{
+    public string name;
+    public double value;
+
+    public Var(string name)
+    {
+        this.name = name;
+    }
+
+    public Var(string name, double value)
+    {
+        this.name = name;
+        this.value = value;
+    }
+
+    public override string ToString()
+    {
+        return name;
+    }
+
+    public static Term operator -(Var v)
+    {
+        return new Term(v, -1);
+    }
+}
+
+[System.Serializable]
+public class Term
+{
+    public int Sign { get; set; }
+    public List<Var> Vars { get; set; }
+    public double Value
+    {
+        get
+        {
+            double x = Sign;
+            for (int i = 0; i < Vars.Count; i++)
+            {
+                x *= Vars[i].value;
+            }
+            return x;
+        }
+    }
+
+    // Constructors
+    public Term(int sign = 1)
+    {
+        this.Sign = sign;
+        Vars = new List<Var>();
+    }
+
+    public Term(Var v, int sign = 1)
+    {
+        this.Sign = sign;
+        Vars = new List<Var>();
+        Vars.Add(v);
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        if (Sign == -1)
+            sb.Append("-");
+        for (int i = 0; i < Vars.Count; i++)
+            sb.Append(Vars[i]);
+        if (Vars.Count == 0)
+            sb.Append("1");
+        return sb.ToString();
+    }
+
+    public static Term operator *(Term a, Term b)
+    {
+        Term c = new Term(a.Sign * b.Sign);
+        c.Vars = a.Vars.Concat(b.Vars).ToList();
+        return c;
+    }
+
+    public static implicit operator Term(Var v)
+    {
+        return new Term(v);
+    }
+}
+
+public class Formula
+{
+    public List<Term> terms;
+
+    public double Value
+    {
+        get
+        {
+            double sum = 0;
+            for (int i = 0; i < terms.Count; i++)
+            {
+                sum += terms[i].Value;
+            }
+            return sum;
+        }
+    }
+
+    public IEnumerable<Var> Vars
+    {
+        get
+        {
+            var vars = new HashSet<Var>();
+            foreach (Term t in terms)
+            {
+                foreach (var v in t.Vars)
+                {
+                    vars.Add(v);
+                }
+            }
+            return vars;
+        }
+    }
+
+    // Constructors
+    public Formula()
+    {
+        terms = new List<Term>();
+    }
+
+    public Formula(Term t)
+    {
+        terms = new List<Term>();
+        terms.Add(t);
+    }
+
+    public Formula(Var t, int sign = 1)
+    {
+        terms = new List<Term>();
+        terms.Add(new Term(t, sign));
+    }
+
+
+    public override string ToString()
+    {
+        if (terms.Count == 0)
+            return "0";
+        var sb = new StringBuilder();
+        for (int i = 0; i < terms.Count; i++)
+        {
+            if (terms[i].Sign > 0 && i != 0)
+                sb.Append("+");
+            sb.Append(terms[i]);
+        }
+        return sb.ToString();
+    }
+
+
+    // Operators
+    public static Formula operator *(Formula a, Formula b)
+    {
+        Formula c = new Formula();
+        foreach (var t1 in a.terms)
+        {
+            foreach (var t2 in b.terms)
+            {
+                c.terms.Add(t1 * t2);
+            }
+        }
+        return c;
+    }
+
+    public static Formula operator +(Formula a, Formula b)
+    {
+        Formula c = new Formula();
+        c.terms = a.terms.Concat(b.terms).ToList();
+        return c;
+    }
+
+    // Static Functions
+    public static Formula Zero => new Formula();
+
+    public static Formula One()
+    {
+        var f = new Formula();
+        f.terms.Add(new Term());
+        return f;
+    }
+
+    public static Formula NegOne()
+    {
+        var f = new Formula();
+        f.terms.Add(new Term(-1));
+        return f;
+    }
+
+    public static implicit operator Formula(Term t)
+    {
+        return new Formula(t);
+    }
+
+    public static implicit operator Formula(Var v)
+    {
+        return new Formula(v);
+    }
+}
+
+public class FormulaMat
+{
+    public int n;
+    public List<List<Formula>> entities;
+
+    // Constructors
+    public FormulaMat(int n = 4)
+    {
+        this.n = n;
+        entities = new List<List<Formula>>();
+        for (int i = 0; i < n; i++)
+        {
+            entities.Add(new List<Formula>());
+            for (int j = 0; j < n; j++)
+            {
+                entities[i].Add(new Formula());
+            }
+        }
+    }
+
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine("[");
+        for (int i = 0; i < entities.Count; i++)
+        {
+            sb.Append("\t");
+            var line = new List<string>();
+            for (int j = 0; j < entities[i].Count; j++)
+            {
+                line.Add(entities[i][j].ToString());
+            }
+            sb.AppendLine(string.Join("\t", line));
+        }
+        sb.Append("]");
+        return sb.ToString();
+    }
+
+    public string ToString(bool eval)
+    {
+        return eval ? ToStringEval() : ToString();
+    }
+
+    private string ToStringEval()
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine("[");
+        for (int i = 0; i < entities.Count; i++)
+        {
+            sb.Append("\t");
+            var line = new List<string>();
+            for (int j = 0; j < entities[i].Count; j++)
+            {
+                line.Add(entities[i][j].Value.ToString());
+            }
+            sb.AppendLine(string.Join("\t", line));
+        }
+        sb.Append("]");
+        return sb.ToString();
+    }
+
+    // Operators
+    public List<Formula> this[int i]
+    {
+        get { return entities[i]; }
+        set { entities[i] = value; }
+    }
+
+    public static FormulaMat operator *(FormulaMat a, FormulaMat b)
+    {
+        var c = new FormulaMat(a.n);
+        for (int i = 0; i < a.n; i++)
+        {
+            for (int j = 0; j < a.n; j++)
+            {
+                for (int k = 0; k < a.n; k++)
+                {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return c;
+    }
+
+    public static Vector4 operator *(FormulaMat mat, Vector4 v)
+    {
+        double[] res = new double[4];
+        for (int i = 0; i < 4; i++)
+        {
+            res[i] = 0;
+            for (int j = 0; j < 4; j++)
+            {
+                res[i] += mat[i][j].Value * v[j];
+            }
+        }
+        return new Vector4((float)res[0], (float)res[1], (float)res[2], (float)res[3]);
+    }
+
+
+
+    // Static Functions
+    public static FormulaMat Identity(int n)
+    {
+        var m = new FormulaMat(n);
+        for (int i = 0; i < n; i++)
+        {
+            m[i][i] = Formula.One();
+        }
+        return m;
+    }
+}
+
